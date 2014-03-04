4ee259b5647d23ce720bcf8ef449d5213feb9a72
jslopeza <jasonlopeza95@gmail.com>
2014-03-04 15:49:25 -0500

    Added router

---
+++ js/router.js
@@ -0,0 +1,27 @@
+/*global Backbone*/
+var app = app || {};
+
+(function(){
+  'use strict';
+
+  //Todo Router
+  //-----------
+
+  var TodoRouter = Backbone.Router.extend({
+    routes: {
+      '*filter': 'setFilter'
+    },
+
+    setFilter: function(param){
+      // Set the current filter to be used
+      app.TodoFilter = param || '';
+
+      // Trigger a collectuon filter event, causing hiding/unhiding
+      // of Todo view items
+      app.todos.trigger('filter');
+    }
+  });
+
+  app.TodoRouter = new TodoRouter();
+  Backbone.history.start();
+})();
