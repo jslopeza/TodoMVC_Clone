c27ce63a1b02543a81a5095907264779e8176101
jslopeza <jasonlopeza95@gmail.com>
2014-03-11 15:33:19 -0400

    Test

--- js/view/todo-view.js
+++ js/view/todo-view.js
@@ -132,4 +132,28 @@ var app = app || {};
 			this.model.destroy();
 		}
 	});
+
+	var view = new Backbone.View.extend({
+  hammerEvents: {
+    'swipeleft window': 'handleSwipe',
+    'tap window': 'handleTap'
+  },
+  hammerOptions: {
+    tap: true
+  },
+  handleSwipe: function(){
+    console.log('Stop.');
+  },
+  handleTap: function(){
+    console.log('Hammer time!');
+  }
+});
+
+app.TodoView.$('window').trigger('swipeleft');
+// → "Stop."
+//view.$('window').trigger('tap');
+// → "Hammer time!"
+
+// Access the view's hammer instance
+app.TodoView.hammer();
 })(jQuery);
