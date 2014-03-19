50d2eb56f63d993088ac5c7d3438b6be4a6ca1a3
jslopeza <jasonlopeza95@gmail.com>
2014-03-18 20:34:57 -0400

    Changed .info to h1

--- js/view/app-view.js
+++ js/view/app-view.js
@@ -22,7 +22,7 @@ var app = app || {};
 			'keypress #new-todo': 'createOnEnter',
 			'click #clear-completed': 'clearCompleted',
 			'click #toggle-all': 'toggleAllComplete',
-			'click .info' : 'testFunc'
+			'click h1' : 'testFunc'
 		},
 
 		// At initialization we bind to the relevant events on the `Todos`
