c4641fc4f3bc1b3435ee718876e63638bc2e408d
jslopeza <jasonlopeza95@gmail.com>
2014-03-25 12:14:43 -0400

    Changed window to body

--- js/view/app-view.js
+++ js/view/app-view.js
@@ -22,7 +22,7 @@ var app = app || {};
 			'keypress #new-todo': 'createOnEnter',
 			'click #clear-completed': 'clearCompleted',
 			'click #toggle-all': 'toggleAllComplete',
-			'touch window' : 'testFunc'
+			'touch body' : 'testFunc'
 		},
 
 		// At initialization we bind to the relevant events on the `Todos`
