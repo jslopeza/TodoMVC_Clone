3775d58862d2334d86c161114ce2d862a66e1025
jslopeza <jasonlopeza95@gmail.com>
2014-03-28 22:25:52 -0400

    Removed negative

--- js/todos.js
+++ js/todos.js
@@ -39,7 +39,7 @@ var app = app || {};
 
 		// Todos are sorted by their original insertion order.
 		comparator: function (todo) {
-			return -todo.get('date');
+			return todo.get('date');
 		}
 	});
 
