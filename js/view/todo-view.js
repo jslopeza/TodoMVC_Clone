f0fde756fb87514d64618f46e8fc8f6430ad26dc
jslopeza <jasonlopeza95@gmail.com>
2014-03-28 20:49:12 -0400

    Changed click to tap

--- js/view/todo-view.js
+++ js/view/todo-view.js
@@ -23,7 +23,7 @@ var app = app || {};
 			'keypress .edit': 'updateOnEnter',
 			'keydown .edit': 'revertOnEscape',
 			'blur .edit': 'close',
-			'click label' : 'testfunc'
+			'tap label' : 'testfunc'
 		},
 
 		// The TodoView listens for changes to its model, re-rendering. Since
