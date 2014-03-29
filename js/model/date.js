a05358d8822da38ae7ae2e06c3d99d003d781416
jslopeza <jasonlopeza95@gmail.com>
2014-03-28 21:26:43 -0400

    Added Date conversion

---
+++ js/model/date.js
@@ -0,0 +1,12 @@
+var app = app || ();
+Backbone.Model.prototype.toJSON = function(){
+return this._parseDates(this.attributes);
+};
+
+Backbone.Model.prototype._parseDates = funtion(attrs){
+attrs = _.clone(attrs);
+var newDate = attrs.dueDate.split(“/“).reverse().join(“/“);
+attrs.dueDate = new Date(newDate).toISOString();
+
+return attars;
+};
