ee44d66120fb094d26aef0f0368364aae6db75a1
jslopeza <jasonlopeza95@gmail.com>
2014-03-28 22:21:37 -0400

    changed listenTo

--- js/view/app-view.js
+++ js/view/app-view.js
@@ -39,7 +39,7 @@ var app = app || {};
 			this.listenTo(app.todos, 'reset', this.addAll);
 			this.listenTo(app.todos, 'change:completed', this.filterOne);
 			this.listenTo(app.todos, 'filter', this.filterAll);
-			this.listenTo(app.todos, 'all', this.render);
+			this.listenTo(app.todos, 'add', this.render);
 
 			// Suppresses 'add' events with {reset: true} and prevents the app view
 			// from being re-rendered for every model. Only renders when the 'reset'
